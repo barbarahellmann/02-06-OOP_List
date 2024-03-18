8ca4465b168a807ef323bc17a30b042e658e5b71
barbarahellmann <babshellmann@web.de>
2024-03-18 14:21:25 +0100

    Added students.get & student.size

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,5 +17,13 @@ public class Main {
         students.add(b);
         students.add(c);
         System.out.println(students);
+        students.add(d);
+        System.out.println(students);
+
+        //students[]
+        System.out.println(students.get(0));
+
+        //students.length
+        System.out.println(students.size());
     }
 }
